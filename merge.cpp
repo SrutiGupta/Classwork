15acfcfd34c2cc680fa725d3c3900eb0f4495af8
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-10 20:53:39 +0530

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -25,8 +25,6 @@ void merge(vector<int>& arr, int left, int mid, int right) {
     while (j < n2)
         arr[k++] = R[j++];
 }
-
-// Recursively divide and sort
 void mergeSort(vector<int>& arr, int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
