180eb15dfd5fdfe54ae1fb555008f350ca264618
Sruti Gupta <156588026+SrutiGupta@users.noreply.github.com>
2025-06-10 20:53:23 +0530

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -5,17 +5,14 @@ using namespace std;
 void merge(vector<int>& arr, int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
-    // Create temporary arrays
+    
     vector<int> L(n1), R(n2);
 
-    // Copy data to temp arrays
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[mid + 1 + j];
-
-    // Merge the temp arrays back into arr
+    
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j])
@@ -23,8 +20,6 @@ void merge(vector<int>& arr, int left, int mid, int right) {
         else
             arr[k++] = R[j++];
     }
-
-    // Copy the remaining elements (if any)
     while (i < n1)
         arr[k++] = L[i++];
     while (j < n2)
